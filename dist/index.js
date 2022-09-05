a1af432cd066373f6689c756165de86db46630f5
Pedro <pedro.henrique@coopersystem.com.br>
2022-09-05 11:21:03 -0300

    :art: First commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const somatorio = (x, y) => {
+    return x + y;
+};
+const somatorioTres = (x, y, z) => {
+    return x + y + z;
+};
+console.log('hello world');
+console.log(somatorio(1, 2));
+console.log(somatorioTres(1, 2, 3));
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const somatorio = (x: number, y: number): number => {
+  return x + y;
+}
+
+const somatorioTres = (x: number, y: number, z: number): number => {
+  return x + y + z;
+}
+
+console.log('hello world');
+console.log(somatorio(1, 4))
+console.log(somatorioTres(1, 2, 5))
